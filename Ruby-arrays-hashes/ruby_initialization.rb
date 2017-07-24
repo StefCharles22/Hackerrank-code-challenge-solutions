39c3730f384fb4e25371b53fb5015032430639d9
stefanie charles <scharles@empatico.org>
2017-07-24 14:28:04 -0400

    array with index part 1

---
+++ Ruby-arrays-hashes/ruby_initialization.rb
@@ -0,0 +1,10 @@
+# Your task is to initialize three different variables as explained below.
+# Initialize an empty array with the variable name array
+# Initialize an array with exactly one nil element in it with the variable name array_1
+# Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+
+array = Array.new()
+
+array_1 = Array.new(1)
+
+array_2 = Array.new(2,10)
